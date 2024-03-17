af509c96e7351e31f2beda778f97b75d9b96fe41
Emre <74266381+Legervad@users.noreply.github.com>
2024-03-17 18:45:49 +0300

    implement environment variables integration

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
-import asyncio
+import os
 
 import discord
 import requests
-
 from discord.ext import commands
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+
 users = []
 bot = commands.Bot(command_prefix="!", case_insensitive=True, intents=discord.Intents.all())
 def get_quote():
@@ -15,7 +18,6 @@ def get_quote():
 @bot.event
 async def on_ready():  # When the bot is ready
     print("We have logged in as {0.user}".format(bot))
-
 
 @bot.event
 async def on_message(message):
@@ -34,11 +36,11 @@ participants = ["emre", "emre2", "emre3", "emre4", "emre5", "emre6", "emre7"]
 async def greet(ctx: discord.ApplicationContext,
                 user: discord.Option(str, choices=participants, description="The user's name"),
                 message: discord.Option(str, "Your message", required=False, default="Hello!")):
-    """A command to greet a user with an optional message."""
+
     currentUser = ctx.user
     greeting = f"{currentUser.name} greets you {user}, with the message {message}!"
     await currentUser.send(greeting)
     await ctx.respond(greeting, ephemeral=True)
 
-bot.run("MTIxMzgwMzY3NDQzNTA2NzkwNA.GFRRpY.NoV9IvIFXThjhzncsEI7ZaxZSi9t_2ttE5GBmc")
+bot.run(os.getenv("TMS_TOKEN"))
 
